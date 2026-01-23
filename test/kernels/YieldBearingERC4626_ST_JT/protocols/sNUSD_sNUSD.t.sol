d970b9bcd603c48761951d2cadda8320d6938071
Ankur Dubey <iam@ankurdubey.com>
2026-01-22 20:03:34 -0800

    fix: snusd funding amount in tests

--- test/kernels/YieldBearingERC4626_ST_JT/protocols/sNUSD_sNUSD.t.sol
+++ test/kernels/YieldBearingERC4626_ST_JT/protocols/sNUSD_sNUSD.t.sol
@@ -39,7 +39,7 @@ contract sNUSD_sNUSD_Test is YieldBearingERC4626_TestBase {
             jtAsset: SNUSD,
             stDecimals: 18,
             jtDecimals: 18,
-            initialFunding: 1_000_000e18 // 1M sNUSD
+            initialFunding: 10_000_000e18 // 10M sNUSD
         });
     }
 
